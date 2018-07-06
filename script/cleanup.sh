f04d6595ac241e271cd4456c6d47dfab0a637dd7
Amanibhavam <iam@defn.sh>
2018-07-06 00:41:19 -0700

    dont minimize

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -46,18 +46,6 @@ echo "==> Clearing last login information"
 >/var/log/wtmp
 >/var/log/btmp
 
-# Whiteout root
-count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
-let count--
-dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count
-rm /tmp/whitespace
-
-# Whiteout /boot
-count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
-let count--
-dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count
-rm /boot/whitespace
-
 echo '==> Clear out swap and disable until reboot'
 set +e
 swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
@@ -74,10 +62,6 @@ if [ "x${swapuuid}" != "x" ]; then
     dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
     /sbin/mkswap -U "${swapuuid}" "${swappart}"
 fi
-
-# Zero out the free space to save space in the final image
-dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
-rm -f /EMPTY
 
 # Make sure we wait until all the data is written to disk, otherwise
 # Packer might quite too early before the large files are deleted
